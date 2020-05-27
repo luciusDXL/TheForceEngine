864ac01a3b346218b9d9f5415760c7a4748afa1c
luciusDXL <ldarkxl@gmail.com>
2020-05-27 15:35:22 -0700

    Moved game source data info into a seperate file.

---
+++ TheForceEngine/TFE_Settings/gameSourceData.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <TFE_System/types.h>
+
+namespace TFE_Settings
+{
+	enum Games
+	{
+		Game_Dark_Forces = 0,
+		Game_Outlaws,
+		Game_Count
+	};
+
+	static const char* c_gameName[] =
+	{
+		"Dark Forces",
+		"Outlaws",
+	};
+
+	// GOG Product IDs:
+	static const char* c_gogProductId[Game_Count] =
+	{
+		"1421404433",	// Game_Dark_Forces
+		"1425302464"	// Game_Outlaws
+	};
+
+	static const char* c_steamLocalPath[Game_Count] =
+	{
+		"dark forces/Game/",	// Game_Dark_Forces
+		"outlaws/Game/",		// Game_Outlaws
+	};
+
+	static const char* c_darkForcesLocations[] =
+	{
+		// C drive
+		"C:/Program Files (x86)/Steam/steamapps/common/dark forces/Game/",
+		"C:/Program Files/Steam/steamapps/common/dark forces/Game/",
+		"C:/Program Files (x86)/GOG.com/Star Wars - Dark Forces/",
+		"C:/GOG Games/Star Wars - Dark Forces/",
+		// D drive
+		"D:/Program Files (x86)/Steam/steamapps/common/dark forces/Game/",
+		"D:/Program Files/Steam/steamapps/common/dark forces/Game/",
+		"D:/Program Files (x86)/GOG.com/Star Wars - Dark Forces/",
+		"D:/GOG Games/Star Wars - Dark Forces/",
+	};
+	static const char* c_outlawsLocations[] =
+	{
+		// C drive
+		"C:/Program Files (x86)/Steam/steamapps/common/outlaws/",
+		"C:/Program Files/Steam/steamapps/common/outlaws/",
+		"C:/Program Files (x86)/GOG.com/outlaws/",
+		"C:/GOG Games/outlaws/",
+		// D drive
+		"D:/Program Files (x86)/Steam/steamapps/common/outlaws/",
+		"D:/Program Files/Steam/steamapps/common/outlaws/",
+		"D:/Program Files (x86)/GOG.com/outlaws/",
+		"D:/GOG Games/outlaws/",
+	};
+	static const u32 c_hardcodedPathCount = TFE_ARRAYSIZE(c_darkForcesLocations);
+
+	static const char** c_gameLocations[] =
+	{
+		c_darkForcesLocations,
+		c_outlawsLocations
+	};
+}
--- TheForceEngine/TFE_Settings/settings.cpp
+++ TheForceEngine/TFE_Settings/settings.cpp
@@ -1,4 +1,5 @@
 #include "settings.h"
+#include "gameSourceData.h"
 #include <TFE_FileSystem/filestream.h>
 #include <TFE_FileSystem/fileutil.h>
 #include <TFE_FileSystem/paths.h>
@@ -13,81 +14,10 @@
 namespace TFE_Settings
 {
 	//////////////////////////////////////////////////////////////////////////////////
-	// Resolution Info
-	//////////////////////////////////////////////////////////////////////////////////
-	// Base Cost: ~2.6 ms / 1M pixels.
-	// 4K only possible with palette conversion during blit in the shader.
-	// Possibly double buffer (+1 frame latency).
-	//s32 w = 320,  h = 200;	// 200p
-	//s32 w = 1440, h = 1080;	//1080p
-	//s32 w = 1920, h = 1440;	//1440p
-	//s32 w = 2880, h = 2160;	//4K
-	//s32 w = 1024, h = 768;
-
-	//////////////////////////////////////////////////////////////////////////////////
 	// Local State
 	//////////////////////////////////////////////////////////////////////////////////
-#define LINEBUF_LEN 1024
-	enum Games
-	{
-		Game_Dark_Forces = 0,
-		Game_Outlaws,
-		Game_Count
-	};
-
-	static const char* c_gameName[]=
-	{
-		"Dark Forces",
-		"Outlaws",
-	};
-
-	// GOG Product IDs:
-	static const char* c_gogProductId[Game_Count]=
-	{
-		"1421404433",	// Game_Dark_Forces
-		"1425302464"	// Game_Outlaws
-	};
-
-	static const char* c_steamLocalPath[Game_Count]=
-	{
-		"dark forces/Game/",	// Game_Dark_Forces
-		"outlaws/Game/",		// Game_Outlaws
-	};
-
-	static const char* c_darkForcesLocations[] =
-	{
-		// C drive
-		"C:/Program Files (x86)/Steam/steamapps/common/dark forces/Game/",
-		"C:/Program Files/Steam/steamapps/common/dark forces/Game/",
-		"C:/Program Files (x86)/GOG.com/Star Wars - Dark Forces/",
-		"C:/GOG Games/Star Wars - Dark Forces/",
-		// D drive
-		"D:/Program Files (x86)/Steam/steamapps/common/dark forces/Game/",
-		"D:/Program Files/Steam/steamapps/common/dark forces/Game/",
-		"D:/Program Files (x86)/GOG.com/Star Wars - Dark Forces/",
-		"D:/GOG Games/Star Wars - Dark Forces/",
-	};
-	static const char* c_outlawsLocations[] =
-	{
-		// C drive
-		"C:/Program Files (x86)/Steam/steamapps/common/outlaws/",
-		"C:/Program Files/Steam/steamapps/common/outlaws/",
-		"C:/Program Files (x86)/GOG.com/outlaws/",
-		"C:/GOG Games/outlaws/",
-		// D drive
-		"D:/Program Files (x86)/Steam/steamapps/common/outlaws/",
-		"D:/Program Files/Steam/steamapps/common/outlaws/",
-		"D:/Program Files (x86)/GOG.com/outlaws/",
-		"D:/GOG Games/outlaws/",
-	};
-	static const u32 c_hardcodedPathCount = TFE_ARRAYSIZE(c_darkForcesLocations);
-
-	static const char** c_gameLocations[] =
-	{
-		c_darkForcesLocations,
-		c_outlawsLocations
-	};
-
+	#define LINEBUF_LEN 1024
+	
 	static char s_settingsPath[TFE_MAX_PATH];
 	static TFE_Settings_Window s_windowSettings = {};
 	static TFE_Settings_Graphics s_graphicsSettings = {};
