57ad59a136544862a5189cefb6a791b4fd37ab06
luciusDXL <ldarkxl@gmail.com>
2020-05-28 11:56:53 -0700

    Cleaned up structures and removed members that are unknown or still being   determined. These still exist in the original reverse-engineered code and   will be added back as needed / decoded. Removed address comments, these are different in 64 bit anyway. :) Added more comments describing what structure members do.

--- TheForceEngine/TFE_Game/RendererClassic/rsector.h
+++ TheForceEngine/TFE_Game/RendererClassic/rsector.h
@@ -9,27 +9,19 @@ struct RWall;
 
 struct RSector
 {
-	s32 u00;			// 0x00
-	s32 u01;			// 0x04
-	s32 u02;			// 0x08
-	s32 prevDrawFrame;	// 0x0c  -- previous frame where the sector was drawn.
-	s32 vertexCount;	// 0x10
-	vec2* verticesWS;	// 0x14
-	vec2* verticesVS;	// 0x18 (this is a pointer, but walls is a guess)
-	s32 wallCount;		// 0x1c
-	RWall* walls;		// 0x20
-	s32 floorHeight;	// 0x24
-	s32 ceilingHeight;	// 0x28
-	s32 secHeight;		// 0x2c	-- final second height (guess, its the same as floorHeight in test cases)
-	s32 u60;			// 0x30
-	s32 u61;			// 0x34
-	s32 u62;			// 0x38
-	s32 ambientFixed;	// 0x3c	 -- fixed point sector ambient.
-	u8  u30[0x20];		// 0x40
-	s32 u4;				// 0x60 = 2?
-	u32* ptr;			// 0x64  -- could be something else.
-	u8  u50[0x18];		// 0x68
-	s32 startWall;		// 0x78
-	s32 drawWallCnt;	// 0x7C
-	u32 flags1;			// 0x80
+	s32 prevDrawFrame;		// previous frame that this sector was drawn/updated.
+	s32 vertexCount;		// number of vertices.
+	vec2* verticesWS;		// world space and view space XZ vertex positions.
+	vec2* verticesVS;
+	s32 wallCount;			// number of walls.
+	RWall* walls;			// wall list.
+	s32 floorHeight;		// floor height (Y); -Y up, larger = lower.
+	s32 ceilingHeight;		// ceiling height (Y); -Y up, smaller = higher.
+	s32 secHeight;			// second height; equals floor height if second height in the data = 0.
+	s32 ambientFixed;		// sector ambient in fixed point in the range of [0.0, 31.0]
+	s32 startWall;			// wall segment start index for rendering
+	s32 drawWallCnt;		// wall segment draw count for rendering
+	u32 flags1;				// sector flags.
+	u32 flags2;
+	u32 flags3;
 };
