412393b1e4699bf75990aac28896426ecd8229ac
luciusDXL <ldarkxl@gmail.com>
2020-09-28 10:12:58 -0700

    Updated the jediRenderer header file with a note clarifying the copyright   situation.

--- TheForceEngine/TFE_JediRenderer/jediRenderer.h
+++ TheForceEngine/TFE_JediRenderer/jediRenderer.h
@@ -1,9 +1,24 @@
 #pragma once
 //////////////////////////////////////////////////////////////////////
-// Renderer - Classic
-// Classic Dark Forces (DOS) derived renderer.
+// Jedi Renderer
+// Classic Dark Forces (DOS) Jedi derived renderer.
+//
+// Copyright note:
+// While the project as a whole is licensed under GPL-2.0, some of the
+// code under TFE_JediRenderer/ was derived from reverse-engineered
+// code from "Dark Forces" (DOS) which is copyrighted by LucasArts.
+// The original renderer is contained in RClassic_Fixed/ and the
+// RClassic_Float/ and RClassic_GPU/ sub-renderers are derived from
+// that code.
+//
+// I consider the reverse-engineering to be "Fair Use" - a means of 
+// supporting the games on other platforms and to improve support on
+// existing platforms without claiming ownership of the games
+// themselves or their IPs.
+//
+// That said using this code in a commercial project is risky without
+// permission of the original copyright holders (LucasArts).
 //////////////////////////////////////////////////////////////////////
-
 #include <TFE_System/types.h>
 #include <TFE_Asset/colormapAsset.h>
 
