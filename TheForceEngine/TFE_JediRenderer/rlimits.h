09517a79968031b8ceea30a7ec854b7455018af3
luciusDXL <ldarkxl@gmail.com>
2020-12-09 00:39:06 -0800

    Cleaned up the comment wording.

--- TheForceEngine/TFE_JediRenderer/rlimits.h
+++ TheForceEngine/TFE_JediRenderer/rlimits.h
@@ -6,7 +6,7 @@ namespace TFE_JediRenderer
 	#define MAX_SEG				384 // Maximum number of wall segments visible in the frame.
 	#define MAX_ADJOIN_SEG		128 // Maximum number of adjoin segements visible in the frame.
 	#define MAX_SPLIT_WALLS		 40 // Maximum number of times walls can be split in a sector.
-	#define MAX_ADJOIN_DEPTH	 40 // Maximum number of adjoin depth - basically how many adjoins you can see through.
+	#define MAX_ADJOIN_DEPTH	 40 // Maximum adjoin depth - basically how many adjoins you can see through.
 	#define MAX_VIEW_OBJ_COUNT	128 // Maximum number of rendered objects in a single sector / view.
 	#define LIGHT_SOURCE_LEVELS	128 // Number of levels in the light source (like the headlamp or weapon fire).
 	#define LIGHT_LEVELS		 32 // Number of light levels, maximum = LIGHT_LEVELS - 1
