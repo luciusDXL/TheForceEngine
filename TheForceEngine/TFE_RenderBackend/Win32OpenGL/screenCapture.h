11d062353221e2bb740f46a25a528107604353b5
luciusDXL <ldarkxl@gmail.com>
2020-12-23 13:21:43 -0800

    Lowered the GIF recording framerate to make them a bit smaller.

--- TheForceEngine/TFE_RenderBackend/Win32OpenGL/screenCapture.h
+++ TheForceEngine/TFE_RenderBackend/Win32OpenGL/screenCapture.h
@@ -48,7 +48,7 @@ private:
 	bool m_recordingStarted;
 	u32  m_recordingFrame;
 
-	f32 m_recordingFramerate = 30.0;
+	f32 m_recordingFramerate = 15.0;
 	s32 m_recordingFrameStart = 0;
 	f64 m_recordingTimeStart = 0.0;
 	f64 m_recordingFrameLast = 0.0;
