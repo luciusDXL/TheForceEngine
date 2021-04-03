ba8c2d0114423608f35900115f55172b84cd7083
luciusDXL <ldarkxl@gmail.com>
2021-04-02 17:30:19 -0700

    * More INF system comment cleanup. * Added system descriptions and reverse-engineering notices to infSystem.h and level.h

--- TheForceEngine/TFE_InfSystem/infSystem.h
+++ TheForceEngine/TFE_InfSystem/infSystem.h
@@ -1,6 +1,20 @@
 #pragma once
 //////////////////////////////////////////////////////////////////////
-// The Force Engine Script System
+// INF System
+// Classic Dark Forces (DOS) Jedi derived INF system.
+//
+// Copyright note:
+// While the project as a whole is licensed under GPL-2.0, some of the
+// code under TFE_InfSystem/ was derived from reverse-engineered
+// code from "Dark Forces" (DOS) which is copyrighted by LucasArts.
+//
+// I consider the reverse-engineering to be "Fair Use" - a means of 
+// supporting the games on other platforms and to improve support on
+// existing platforms without claiming ownership of the games
+// themselves or their IPs.
+//
+// That said using this code in a commercial project is risky without
+// permission of the original copyright holders (LucasArts).
 //////////////////////////////////////////////////////////////////////
 #include "infPublicTypes.h"
 #include <TFE_System/types.h>
@@ -15,32 +29,35 @@ namespace TFE_InfSystem
 	void shutdown();
 	
 	// ** Per-frame update functions **
+	// These get called (potentially) every frame and update the current INF state or
+	// state of objects interacting with the system.
+
 	// Per elevator frame update.
 	void update_elevators();
 	// Per frame teleport update.
 	void update_teleports();
-
+	
 	// ** Runtime API **
-	// Send messages so that entities and the player can interact with the INF system.
-	// If msgType = IMSG_SET/CLEAR_BITS the msg is processed directly for this wall OR
-	// this iterates through the valid links and calls their msgFunc.
+	// Messages are the way entities and the player interact with the INF system during gameplay.
+
+	// Send a message to a specific wall or line, such as when "nudging" a switch or crossing a line.
 	// wall:    message target.
 	// entity:  entity involved (such as the player) or nullptr.
-	// evt:     event(s) (see InfEventMask above).
+	// evt:     event(s) (see InfEventMask  in "infPublicTypes.h" such as INF_EVENT_CROSS_LINE_FRONT).
 	// msgType: message type (such as IMSG_TRIGGER).
 	void inf_wallSendMessage(RWall* wall, SecObject* entity, u32 evt, InfMessageType msgType);
 
-	// Send messages so that entities and the player can interact with the INF system.
-	// If msgType = IMSG_SET/CLEAR_BITS, IMSG_MASTER_ON/OFF, IMSG_WAKEUP it is processed directly for this sector AND
-	// this iterates through the valid links and calls their msgFunc.
-	// For evt, see InfEventMask above.
+	// Send a message to a sector, such as entering, leaving, or "nudging".
 	// sector:  message target.
 	// entity:  entity involved (such as the player) or nullptr.
-	// evt:     event(s) (see InfEventMask above).
+	// evt:     event(s) (see InfEventMask in "infPublicTypes.h" such as INF_EVENT_ENTER_SECTOR).
 	// msgType: message type (such as IMSG_TRIGGER).
 	void inf_sectorSendMessage(RSector* sector, SecObject* entity, u32 evt, InfMessageType msgType);
-
+	
 	// ** Loadtime API **
+	// These functions are used at load-time to setup special elevators based on sector flags or load
+	// the level INF.
+
 	// Used when loading levels.
 	struct InfElevator;
 
--- TheForceEngine/TFE_Level/level.h
+++ TheForceEngine/TFE_Level/level.h
@@ -1,6 +1,22 @@
 #pragma once
 //////////////////////////////////////////////////////////////////////
-// Sector
+// Level
+// Classic Dark Forces (DOS) Jedi derived Level structures and shared
+// functionality used by systems such as collision detection,
+// rendering, and INF.
+//
+// Copyright note:
+// While the project as a whole is licensed under GPL-2.0, some of the
+// code under TFE_Level/ was derived from reverse-engineered
+// code from "Dark Forces" (DOS) which is copyrighted by LucasArts.
+//
+// I consider the reverse-engineering to be "Fair Use" - a means of 
+// supporting the games on other platforms and to improve support on
+// existing platforms without claiming ownership of the games
+// themselves or their IPs.
+//
+// That said using this code in a commercial project is risky without
+// permission of the original copyright holders (LucasArts).
 //////////////////////////////////////////////////////////////////////
 #include <TFE_System/types.h>
 #include <TFE_System/memoryPool.h>
