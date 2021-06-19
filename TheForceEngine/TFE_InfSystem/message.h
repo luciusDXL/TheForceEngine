a48719bbfecea82d81a3558e07b7860054021db7
luciusDXL <ldarkxl@gmail.com>
2021-06-19 13:21:11 -0700

    Minor cleanup to message header.

--- TheForceEngine/TFE_InfSystem/message.h
+++ TheForceEngine/TFE_InfSystem/message.h
@@ -10,49 +10,48 @@
 #include <vector>
 #include <string>
 
-struct RSector;
-
-struct MessageAddress
+enum MessageType
 {
-	char name[16];		// 0x00
-	s32  param0;		// 0x10
-	s32  param1;		// 0x14
-	RSector* sector;	// 0x18
+	MSG_FREE       = 1,
+	MSG_TRIGGER    = 7,
+	MSG_NEXT_STOP  = 8,
+	MSG_PREV_STOP  = 9,
+	MSG_GOTO_STOP  = 11,
+	MSG_REV_MOVE   = 12,
+	MSG_DONE       = 21,
+	MSG_DAMAGE     = 22,
+	MSG_EXPLOSION  = 23,
+	MSG_WAKEUP     = 25,
+	MSG_MASTER_ON  = 29,
+	MSG_MASTER_OFF = 30,
+	MSG_SET_BITS   = 31,
+	MSG_CLEAR_BITS = 32,
+	MSG_COMPLETE   = 33,
+	MSG_LIGHTS     = 34,
+	MSG_COUNT
 };
 
 typedef void(*MessageFunc)(void*);
-
-enum MessageType
+struct MessageAddress
 {
-	MSG_FREE = 1,		// Internal Only - delete the InfTrigger or InfElevator.
-	MSG_TRIGGER = 7,
-	MSG_NEXT_STOP = 8,
-	MSG_PREV_STOP = 9,
-	MSG_GOTO_STOP = 11,
-	MSG_REV_MOVE = 12,
-	MSG_DONE = 21,
-	MSG_DAMAGE = 22,
-	MSG_EXPLOSION = 23,
-	MSG_WAKEUP = 25,
-	MSG_MASTER_ON = 29,
-	MSG_MASTER_OFF = 30,
-	MSG_SET_BITS = 31,
-	MSG_CLEAR_BITS = 32,
-	MSG_COMPLETE = 33,
-	MSG_LIGHTS = 34,
-	MSG_COUNT
+	char name[16];
+	s32  param0;
+	s32  param1;
+	RSector* sector;
 };
 
 namespace TFE_Message
 {
+	// Add and retrieve addresses, this way named sectors can be accessed without looping through the entire set.
 	void message_addAddress(const char* name, s32 param0, s32 param1, RSector* sector);
 	MessageAddress* message_getAddress(const char* name);
 	void message_free();
 
+	// Send a message to either an object or sector.
 	void message_sendToObj(SecObject* obj, MessageType msgType, MessageFunc func);
 	void message_sendToSector(RSector* sector, SecObject* entity, u32 evt, MessageType msgType);
 
-	// Optional message values.
+	// Optional message values - set these before calling message_XXX() and read inside the message function.
 	extern void* s_msgEntity;
 	extern void* s_msgTarget;
 	extern u32 s_msgArg1;
