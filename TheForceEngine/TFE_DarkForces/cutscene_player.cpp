56cedfbdce6ffb4cfc7eeb624ebf3451abdcac5f
luciusDXL <ldarkxl@gmail.com>
2021-11-24 14:37:12 -0800

    Integrating cutscene code. * Added the cutscene system. * Added the cutscene player (stubbed for now).

---
+++ TheForceEngine/TFE_DarkForces/cutscene_player.cpp
@@ -0,0 +1,19 @@
+#include "cutscene_player.h"
+#include "util.h"
+#include <TFE_Game/igame.h>
+#include <TFE_System/system.h>
+#include <TFE_Jedi/Math/core_math.h>
+#include <TFE_Jedi/Renderer/virtualFramebuffer.h>
+#include <TFE_FileSystem/filestream.h>
+#include <TFE_System/parser.h>
+
+using namespace TFE_Jedi;
+
+namespace TFE_DarkForces
+{
+	JBool cutscenePlayer_update()
+	{
+		// STUBBED
+		return JFALSE;
+	}
+}  // TFE_DarkForces
