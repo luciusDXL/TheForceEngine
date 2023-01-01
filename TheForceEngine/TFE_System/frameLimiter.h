e010585c6afdd1e58f214350b236c9b0eb525795
luciusDXL <ldarkxl@gmail.com>
2023-01-01 16:44:35 +0100

    Frame limiter code (not hooked up yet).

---
+++ TheForceEngine/TFE_System/frameLimiter.h
@@ -0,0 +1,18 @@
+#pragma once
+//////////////////////////////////////////////////////////////////////
+// The Force Engine System Library
+// System functionality, such as timers and logging.
+//////////////////////////////////////////////////////////////////////
+
+#include "system.h"
+
+namespace TFE_System
+{
+	// Set the frame limit in Frames Per Second (FPS).
+	// A value of 0 sets no limit.
+	void frameLimiter_set(f64 limitFPS = 0.0);
+	f64 frameLimiter_getAccuracy();
+
+	void frameLimiter_begin();
+	void frameLimiter_end();
+}
