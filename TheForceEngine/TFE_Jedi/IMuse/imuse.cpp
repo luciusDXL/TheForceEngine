a9933d336c3aef1f519dbdcbbe6c9c8707cc0dbb
luciusDXL <ldarkxl@gmail.com>
2023-07-04 15:20:53 -0700

    Replaced std::min() with min().

--- TheForceEngine/TFE_Jedi/IMuse/imuse.cpp
+++ TheForceEngine/TFE_Jedi/IMuse/imuse.cpp
@@ -1296,7 +1296,7 @@ namespace TFE_Jedi
 
 	void ImHandleChannelPriorityChange(ImMidiPlayer* player, ImMidiOutChannel* channel)
 	{
-		channel->priority = std::min(channel->partPriority + player->priority, 127);
+		channel->priority = min(channel->partPriority + player->priority, 127);
 		if (channel->data)
 		{
 			ImMidiChannelSetPriority(channel->data, channel->priority);
