d7baed172d6a7615b8eeca33c37892715326778c
Kevin Foley <kevinfoley@users.noreply.github.com>
2024-11-01 00:15:56 -0700

    dragon, phaseOne: replace `anim.state` magic numbers with enum values; rename some functions to reflect the purpose of the function (e.g. renamed `kellDragon_handleState4()` to `kellDragon_handleAttack()`) (#284)

--- TheForceEngine/TFE_DarkForces/Actor/phaseOne.h
+++ TheForceEngine/TFE_DarkForces/Actor/phaseOne.h
@@ -9,6 +9,16 @@
 
 namespace TFE_DarkForces
 {
+	enum PhaseOneStates
+	{
+		P1STATE_DEFAULT = 0,
+		P1STATE_CHARGE,
+		P1STATE_WANDER,
+		P1STATE_ATTACK,
+		P1STATE_DYING,
+		P1STATE_SEARCH,
+	};
+
 	void phaseOne_exit();
 	void phaseOne_precache();
 	Logic* phaseOne_setup(SecObject* obj, LogicSetupFunc* setupFunc);
