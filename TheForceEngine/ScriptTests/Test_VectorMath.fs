823240579101536bda0a1a44cef407fc8814d79a
luciusDXL <ldarkxl@gmail.com>
2024-12-14 10:10:59 -0800

    * Added array indexing tests for float2. * Added more math function tests for float2.

--- TheForceEngine/ScriptTests/Test_VectorMath.fs
+++ TheForceEngine/ScriptTests/Test_VectorMath.fs
@@ -15,6 +15,9 @@ void test_float2_swizzle()
 	// float2 -> scalar swizzles.
 	test.expectEq(a.x, 1.0f);
 	test.expectEq(a.y, 2.0f);
+	// float2 array indexing.
+	test.expectEq(a[0], 1.0f);
+	test.expectEq(a[1], 2.0f);
 }
 
 // float2 x float2 math
@@ -30,8 +33,6 @@ void test_float2_float2_math()
 	test.expectEq(a / b, float2(1.0/3.0, 2.0/4.0));
 
 	const float neg45 = math.radians(-45.0); // -45 degrees in radians.
-	// Only concern ourselves with math functions that effect vector types,
-	// scalar/per-component functions should be tested separately.
 	test.expectEq(math.distance(a, b), 2.828427125f);
 	test.expectEq(math.dot(a, b), 11.0f);
 	test.expectEq(math.length(a), 2.2360679775f);
@@ -39,6 +40,15 @@ void test_float2_float2_math()
 	test.expectEq(math.perp(a, b), -2.0f);
 	test.expectEq(math.sincos(neg45), float2(-0.7071067812, 0.7071067812));
 	
+	test.expectEq(math.clamp(a, float2(0.5,2.5), float2(2,3)), float2(1.0, 2.5));
+	test.expectEq(math.clamp(a, 1.5, 3.0), float2(1.5, 2.0));
+	test.expectEq(math.max(a, b), float2(3, 4));
+	test.expectEq(math.max(a, 1.5), float2(1.5, 2));
+	test.expectEq(math.min(a, b), float2(1, 2));
+	test.expectEq(math.min(a, 1.5), float2(1, 1.5));
+	test.expectEq(math.mix(a, b, 0.5), float2(2, 3));
+	test.expectEq(math.mix(a, b, float2(0.5, 1.0)), float2(2, 4));
+
 	test.expectTrue(math.all(a));
 	test.expectTrue(math.any(a));
 	test.expectFalse(math.all(c));
