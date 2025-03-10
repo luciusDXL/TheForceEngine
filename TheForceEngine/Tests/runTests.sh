8a98030624c7c9bdbfba32ce780788f906e0ee1b
Karjala22 <78927981+Karjala22@users.noreply.github.com>
2025-03-09 23:33:55 -0400

    more workflow testing

--- TheForceEngine/Tests/runTests.sh
+++ TheForceEngine/Tests/runTests.sh
@@ -1,11 +1,14 @@
-# TFE Does not like ~ so lets expand it
+# Setup the root path
+root_path="$1"
+
+# TFE does not like tildes in paths
 user_path=~
 
 # Run the demo and generate new log file
-${{github.workspace}}/build/theforceengine -gDark -r${{github.workspace}}/build/TheForceEngine/Tests/Replays/base_test.demo --demo_logging --exit_after_replay
+$root_path/build/theforceengine -gDark -r$root_path/TheForceEngine/Tests/Replays/base_test.demo --demo_logging --exit_after_replay
 
 # Diff Results of the rest 
-result=`diff <(tail -n 1 $user_path/.local/share/TheForceEngine/replay.log) <(tail -n 1 $user_path/tfe/TheForceEngine/Tests/Replays/base_replay.log)`
+result=`diff <(tail -n 1 $user_path/.local/share/TheForceEngine/replay.log) <(tail -n 1 $root_path/TheForceEngine/Tests/Replays/base_replay.log)`
 
 if $result > /dev/null; then
     exit 0
