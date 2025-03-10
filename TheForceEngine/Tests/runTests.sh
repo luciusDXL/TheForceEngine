f86f99da89eaf22eab63e626c5e50eb6a92839bc
Karjala22 <78927981+Karjala22@users.noreply.github.com>
2025-03-09 23:48:04 -0400

    more workflow test

--- TheForceEngine/Tests/runTests.sh
+++ TheForceEngine/Tests/runTests.sh
@@ -4,8 +4,14 @@ root_path="$1"
 # TFE does not like tildes in paths
 user_path=~
 
+ls -tral $root_path
+
+ls -tral $root_path/TheForceEngine/
+
 # Run the demo and generate new log file
 $root_path/build/theforceengine -gDark -r$root_path/TheForceEngine/Tests/Replays/base_test.demo --demo_logging --exit_after_replay
+
+echo $?
 
 # Diff Results of the rest 
 result=`diff <(tail -n 1 $user_path/.local/share/TheForceEngine/replay.log) <(tail -n 1 $root_path/TheForceEngine/Tests/Replays/base_replay.log)`
