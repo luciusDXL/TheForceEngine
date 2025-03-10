7d7408bf3544797839739043c0b19f75c152af66
Karjala22 <78927981+Karjala22@users.noreply.github.com>
2025-03-10 00:15:08 -0400

    updating

--- TheForceEngine/Tests/runTests.sh
+++ TheForceEngine/Tests/runTests.sh
@@ -8,14 +8,18 @@ ls -tral $root_path
 
 ls -tral $root_path/TheForceEngine/
 
+ls -tral $root_path/TheForceEngine/Tests/
+
+ldd $root_path/TheForceEngine/theforceengine
+
 # Run the demo and generate new log file
 echo "running tfe"
-$root_path/build/theforceengine -gDark -r$root_path/TheForceEngine/Tests/Replays/base_test.demo --demo_logging --exit_after_replay
+$root_path/TheForceEngine/theforceengine -gDark -r$root_path/Tests/Replays/base_test.demo --demo_logging --exit_after_replay
 echo "Result is "$?
 echo "Done running tfe"
 
 # Diff Results of the rest 
-result=`diff <(tail -n 1 $user_path/.local/share/TheForceEngine/replay.log) <(tail -n 1 $root_path/TheForceEngine/Tests/Replays/base_replay.log)`
+result=`diff <(tail -n 1 $user_path/.local/share/TheForceEngine/replay.log) <(tail -n 1 $root_path/Tests/Replays/base_replay.log)`
 
 if $result > /dev/null; then
     echo "RESULTS MATCH!"
