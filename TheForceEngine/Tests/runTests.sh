d7af3ddab5598a208079a24f329afe4c4c98377c
Karjala22 <78927981+Karjala22@users.noreply.github.com>
2025-03-10 00:56:01 -0400

    test

--- TheForceEngine/Tests/runTests.sh
+++ TheForceEngine/Tests/runTests.sh
@@ -8,7 +8,7 @@ ls -tral $root_path
 
 ls -tral $root_path/TheForceEngine/
 
-ls -tral $root_path/TheForceEngine/Tests/
+ls -tral $root_path/TheForceEngine/Tests/Replays/
 
 ldd $root_path/TheForceEngine/theforceengine
 
@@ -18,12 +18,24 @@ $root_path/TheForceEngine/theforceengine -gDark -r$root_path/Tests/Replays/base_
 echo "Result is "$?
 echo "Done running tfe"
 
-find / -type d -iname the_force_engine_log.txt 2>/dev/null | xargs cat
+if [ -f $root_path/TheForceEngine/theforceengine ]; then
+   echo "dump exists"
+else
+   echo "no dump exists"
+fi 
+
+echo "running tfe2"
+$root_path/theforceengine -gDark -r$root_path/Tests/Replays/base_test.demo --demo_logging --exit_after_replay
+echo "Result is "$?
+echo "Done running tfe"
+
+
+#find / -type d -iname the_force_engine_log.txt 2>/dev/null | xargs cat
 
 top -b -n 1 | head -n 20
 
 # Diff Results of the rest 
-result=`diff <(tail -n 1 $user_path/.local/share/TheForceEngine/replay.log) <(tail -n 1 $root_path/Tests/Replays/base_replay.log)`
+result=`diff <(tail -n 1 $user_path/.local/share/TheForceEngine/replay.log) <(tail -n 1 $root_path/TheForceEngine/Tests/Replays/base_replay.log)`
 
 if $result > /dev/null; then
     echo "RESULTS MATCH!"
