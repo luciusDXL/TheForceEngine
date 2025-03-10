ebd2126f23ca5da2d670b926cfc2ad2e1f832bfa
Karjala22 <78927981+Karjala22@users.noreply.github.com>
2025-03-10 00:05:58 -0400

    testing

--- TheForceEngine/Tests/runTests.sh
+++ TheForceEngine/Tests/runTests.sh
@@ -9,16 +9,19 @@ ls -tral $root_path
 ls -tral $root_path/TheForceEngine/
 
 # Run the demo and generate new log file
+echo "running tfe"
 $root_path/build/theforceengine -gDark -r$root_path/TheForceEngine/Tests/Replays/base_test.demo --demo_logging --exit_after_replay
-
-echo $?
+echo "Result is "$?
+echo "Done running tfe"
 
 # Diff Results of the rest 
 result=`diff <(tail -n 1 $user_path/.local/share/TheForceEngine/replay.log) <(tail -n 1 $root_path/TheForceEngine/Tests/Replays/base_replay.log)`
 
 if $result > /dev/null; then
+    echo "RESULTS MATCH!"
     exit 0
 else
+    echo "RESULTS DO NOT MATCH!"
     echo $result
     exit 1
 fi
