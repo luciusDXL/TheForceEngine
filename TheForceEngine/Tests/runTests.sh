cc3832bd347447079b1aff333105ac050cae43be
Karjala22 <78927981+Karjala22@users.noreply.github.com>
2025-03-09 23:21:33 -0400

    more bug fixing

--- TheForceEngine/Tests/runTests.sh
+++ TheForceEngine/Tests/runTests.sh
@@ -2,7 +2,7 @@
 user_path=~
 
 # Run the demo and generate new log file
-$user_path/tfe/theforceengine -gDark -r$user_path/tfe/TheForceEngine/Tests/Replays/base_test.demo --demo_logging --exit_after_replay
+${{github.workspace}}/build/theforceengine -gDark -r${{github.workspace}}/build/TheForceEngine/Tests/Replays/base_test.demo --demo_logging --exit_after_replay
 
 # Diff Results of the rest 
 result=`diff <(tail -n 1 $user_path/.local/share/TheForceEngine/replay.log) <(tail -n 1 $user_path/tfe/TheForceEngine/Tests/Replays/base_replay.log)`
